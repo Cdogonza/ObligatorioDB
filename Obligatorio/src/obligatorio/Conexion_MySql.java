fb85f6c77ed2a0de596f780b88f0c3d1a7d19641
Cdogonza <gonzalo.ucu@correo.ucu.edu.uy>
2022-11-10 15:37:22 -0300

    Initial Commit

---
+++ Obligatorio/src/obligatorio/Conexion_MySql.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package obligatorio;
+
+/**
+ *
+ * @author gpaz1
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Conexion_MySql {
+
+
+    public Statement sentencia;
+    public ResultSet resultado;
+public boolean conectado=false;
+private Connection conec;
+
+    public Connection ConectarBasedeDatos() {
+        try {
+
+            final String Controlador = "com.mysql.cj.jdbc.Driver";
+            Class.forName(Controlador);
+            final String url_bd = "jdbc:mysql://localhost:3306/obligatorioDB?useUnicode=true&use"
+                    + "JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
+                    + "serverTimezone=UTC";
+            conec = DriverManager.getConnection(url_bd, "gonzalo", "admin");
+
+            sentencia = conec.createStatement();
+            conectado = true;
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            //   JOptionPane.showMessageDialog(null, ex.getMessage(), "Error ", JOptionPane.ERROR_MESSAGE);
+            System.out.println(ex.getMessage());
+        }
+        return conec;
+    }
+
+    public void DesconectarBasedeDatos() {
+        try {
+            if (conec != null) {
+                if (sentencia != null) {
+                    sentencia.close();
+                }
+                conec.close();
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+
+    public Connection getConnection() {
+        return conec;
+    }
+}
---
+++ Obligatorio/src/obligatorio/Obligatorio.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package obligatorio;
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gpaz1
+ */
+public class Obligatorio {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conexion_MySql conexion = new Conexion_MySql();
+        conexion.ConectarBasedeDatos();
+        if(conexion.conectado){
+            conexion.DesconectarBasedeDatos();
+            Loggin cargaLoggin = new Loggin();
+            cargaLoggin.setVisible(true);
+            System.out.println("BIENVENIDO");
+        }else{
+            JOptionPane.showMessageDialog(null,"Ha surgido un error de conexion a la base de datos, contacte con el Administrador","Error de Conexion",JOptionPane.ERROR_MESSAGE);
+         
+        }
+        
+
+
+ 
+    }
+}
